a7b24200607018e37453af6a479aa93718edb9f2
Josue <Josue@DESKTOP-VDKQOTF>
2024-04-10 16:08:43 -0300

    Realizacion del ejercicio 1

---
+++ tp1_JosueDuran/src/ar/edu/unju/fi/ejercicio1/Ejercicio1.java
@@ -0,0 +1,18 @@
+package ar.edu.unju.fi.ejercicio1;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		
+		int num1 = 8;
+		int num2 = 5;
+		int num3 = 1;
+		int num4 = 20;
+		int num5 = 9;
+		
+		float promedio = (num1+num2+num3+num4+num5)/5f;
+		System.out.println("El promedio es: " +promedio);
+		
+	}
+
+}
