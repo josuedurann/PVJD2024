3b7a9e91dae0c5ef4c59950fdde828ab04b2d020
Josue <Josue@DESKTOP-VDKQOTF>
2024-04-19 19:41:59 -0300

    Realizacion del ejercicio 11

---
+++ tp1_JosueDuran/src/ar/edu/unju/fi/ejercicio11/Main.java
@@ -0,0 +1,17 @@
+package ar.edu.unju.fi.ejercicio11;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+
+			        int suma=80;
+			        
+			        for(int i=2,j=40;j>0;j--)
+			        {
+			        i++;
+			        System.out.print(suma+" ");
+			        suma+=(j-i);
+        }
+    }
+}
